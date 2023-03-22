191816acd6d047cc39298b4b8101f589b8a14db9
Yorin <S1090402@student.windesheim.nl>
2023-03-22 20:36:14 +0100

    added main homepage, and added UserRepository

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class IndexController extends AbstractController
+{
+    public function index(): Response
+    {
+        return new Response(
+            'test'
+        );
+    }
+}
