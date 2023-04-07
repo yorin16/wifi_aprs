732cdf909bab96e2ca74e408cf7e5903217fb85e
Yorin <S1090402@student.windesheim.nl>
2023-04-07 17:05:18 +0200

    added add user button. added delete with confirmation screen. removed RegistrationController.php no longer needed.

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -4,19 +4,23 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use App\Form\EditUserType;
+use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
 {
 
-    public function __construct(private UserRepository $userRepository, private EntityManagerInterface $entityManager)
-    {
-    }
+    public function __construct(
+        private UserRepository $userRepository,
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $userPasswordHasher
+    ){}
 
     public function index(): Response
     {
@@ -24,6 +28,32 @@ class UserController extends AbstractController
 
         return $this->render('admin/user/index.html.twig', [
             'users' => $users
+        ]);
+    }
+
+    public function add(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $this->userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('admin_user');
+        }
+
+        return $this->render('admin/user/add.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 
@@ -41,6 +71,26 @@ class UserController extends AbstractController
 
         return $this->render('admin/user/edit.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    public function delete(User $user, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete_user_' . $user->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'User deleted');
+        } else {
+            $this->addFlash('error', 'Invalid CSRF token');
+        }
+
+        return $this->redirectToRoute('admin_user');
+    }
+
+    public function confirmDelete(User $user): Response
+    {
+        return $this->render('admin/user/confirm_delete.html.twig', [
+            'user' => $user,
         ]);
     }
 }
