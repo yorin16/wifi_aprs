5b2110e9a6a3ce46669b42557d93151625136f42
Yorin <S1090402@student.windesheim.nl>
2023-08-26 17:13:18 +0200

    pr style changes

--- migrations/Version20230409161029.php
+++ migrations/Version20230409161029.php
@@ -23,7 +23,6 @@ final class Version20230409161029 extends AbstractMigration
         $this->addSql('ALTER TABLE location DROP FOREIGN KEY FK_5E9E89CB94A4C7D4');
         $this->addSql('ALTER TABLE device CHANGE guid guid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\'');
         $this->addSql('ALTER TABLE device CHANGE disabled disabled TINYINT(1) DEFAULT 0 NOT NULL');
-        // $this->addSql('ALTER TABLE device CHANGE id id INT NOT NULL, CHANGE guid guid VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CB94A4C7D4 FOREIGN KEY (device_id) REFERENCES device (id) ON DELETE SET NULL');
     }
 
@@ -33,7 +32,6 @@ final class Version20230409161029 extends AbstractMigration
         $this->addSql('ALTER TABLE location DROP FOREIGN KEY FK_5E9E89CB94A4C7D4');
         $this->addSql('ALTER TABLE device CHANGE guid guid CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\'');
         $this->addSql('ALTER TABLE device CHANGE disabled disabled TINYINT(1) NOT NULL');
-        // $this->addSql('ALTER TABLE device CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE guid guid CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:guid)\'');
         $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CB94A4C7D4 FOREIGN KEY (device_id) REFERENCES device (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
     }
 }
