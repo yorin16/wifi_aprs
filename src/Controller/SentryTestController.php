5251351dda722d5641e3046f1a7007308569f376
Yorin <S1090402@student.windesheim.nl>
2023-10-05 21:13:38 +0200

    sentry test

--- src/Controller/SentryTestController.php
+++ src/Controller/SentryTestController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SentryTestController extends AbstractController
 {
+    /**
+     * @throws Exception
+     */
     public function index()
     {
         //TODO: remove this test controller
-        throw new NotFoundHttpException('test');
+        throw new Exception('test');
     }
 }
